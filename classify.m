8e55ef46b47413fc23434bb7cd0c55990b0dfbb4
itsmeknt <itsmeknt@gmail.com>
2013-05-28 15:46:43 -0600

    added evaluation metrics

--- classify.m
+++ classify.m
@@ -1,4 +1,4 @@
-function [  ] = classify(dataset_base_dir, feature_cache_dir, use_feature_cache)
+function [mean_accuracy confusion_matrix] = classify(dataset_base_dir, feature_cache_dir, use_feature_cache)
 
 % dataset_base_dir: the base directory for the image files
 % feature_cache_dir: the base directory for the data files that are generated
@@ -27,7 +27,7 @@ num_files = size(fnames,1);
 filenames = cell(num_files,1);
 
 for f = 1:num_files
-	filenames{f} = fnames(f).name;
+   enames{f} = fnames(f).name;
 end
 
 % train - generate dictionary, learn svm model
@@ -39,5 +39,42 @@ model = train(training_labels, training_features);
 [testing_features testing_labels] = compute_features(filenames, dataset_test_dir, feature_cache_test_dir, use_feature_cache);
 [predicted_label, accuracy, decision_values/prob_estimates] = predict(testing_labels, testing_features, model);
 
+
+%% TODO: split to separate file: get_class_label in compilepyramid, get_class_accuracies and get_confusion_matrix in classify
+[class_accuracies class_list] = get_class_accuracies(predicted_label, testing_labels);
+mean_accuracy =  mean(class_accuracies);
+[confusion_matrix class_list2] = get_confusion_matrix(predicted_label, testing_labels);
+
+ASSERT_TRUE = class_list==class_list2
 end
 
+function [class_accuracies class_list] = get_class_accuracies(predicted_label, true_label)
+class_list = unique([predicted_label; true_label]);
+class_accuracies = zeros(length(class_list), 1);
+for i=1:length(class_list)
+   curr_class = class_list(i);
+   true_class_binary = (true_label == curr_class);
+   num_class = sum(true_class_binary);
+   
+   predicted_class_binary = (predicted_label == curr_class);
+   num_correct_class = sum(predicted_class_binary & true_class_binary);
+   
+   class_accuracies(i) = num_correct_class/num_class;
+end
+end
+
+function [confusion_matrix, class_list] = get_confusion_matrix(predicted_label, true_label)
+class_list = unique([predicted_label; true_label]);
+confusion_matrix = zeros(length(class_list), length(class_list));
+for i=1:length(class_list)
+   curr_true_class = class_list(i);
+   true_class_binary = (true_label == curr_true_class);
+   
+   for j=1:length(class_list)
+      curr_predicted_class = class_list(i);
+      predicted_class_binary = (predicted_label == curr_predicted_class);
+      
+      confusion_matrix[i][j] = sum(true_class_binary & predicted_class_binray);
+   end
+end
+end
