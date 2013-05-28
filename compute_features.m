6fbeb757ddea1aef4fd0ab3d4dd3bac3077dcea2
itsmeknt <itsmeknt@gmail.com>
2013-05-28 15:13:15 -0600

    Update compute_features.m

--- compute_features.m
+++ compute_features.m
@@ -1,6 +1,8 @@
-function [pyramid_all] = compute_features(imageFileList, dataBaseDir, canSkip)
+function [pyramid_all] = compute_features(imageFileList, imageBaseDir, dataBaseDir, canSkip)
+
 config;
 
+GenerateSiftDescriptors(imageFileList,imageBaseDir,dataBaseDir,maxImageSize,gridSpacing,patchSize,canSkip);
 BuildHistograms(imageFileList,dataBaseDir,'_sift.mat',dictionarySize,canSkip);
 pyramid_all = CompilePyramid(imageFileList,dataBaseDir,sprintf('_texton_ind_%d.mat',dictionarySize),dictionarySize,pyramidLevels,canSkip);
 
