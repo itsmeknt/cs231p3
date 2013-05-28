c714bb79f07d9ae94c672380bc841686b6017306
itsmeknt <itsmeknt@gmail.com>
2013-05-28 15:24:13 -0600

    now returns class label

--- compute_features.m
+++ compute_features.m
@@ -1,10 +1,10 @@
-function [pyramid_all] = compute_features(imageFileList, imageBaseDir, dataBaseDir, canSkip)
+function [pyramid_all class_label_all] = compute_features(imageFileList, imageBaseDir, dataBaseDir, canSkip)
 
 config;
 
 GenerateSiftDescriptors(imageFileList,imageBaseDir,dataBaseDir,maxImageSize,gridSpacing,patchSize,canSkip);
 BuildHistograms(imageFileList,dataBaseDir,'_sift.mat',dictionarySize,canSkip);
-pyramid_all = CompilePyramid(imageFileList,dataBaseDir,sprintf('_texton_ind_%d.mat',dictionarySize),dictionarySize,pyramidLevels,canSkip);
+[pyramid_all class_label_all] = CompilePyramid(imageFileList,dataBaseDir,sprintf('_texton_ind_%d.mat',dictionarySize),dictionarySize,pyramidLevels,canSkip);
 
 end
 
